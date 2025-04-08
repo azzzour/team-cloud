e6d433f502ed85d8e7e7f5f0972321798121c340
azzzour <dr.alik-2002@mail.ru>
2025-04-08 16:52:35 +0400

    feat: Add security to storage service

--- storage-service/src/main/java/com/alikgizatulin/storageapp/config/SecurityConfig.java
+++ storage-service/src/main/java/com/alikgizatulin/storageapp/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.alikgizatulin.storageapp.config;
 
+import com.alikgizatulin.commonlibrary.security.KeycloakJwtAuthenticationConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,8 +14,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests( request ->
-                        request.anyRequest().permitAll())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/v1/storage/**").authenticated()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServe ->
+                        oAuth2ResourceServe.jwt(jwtConfigurer ->
+                                jwtConfigurer.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter()))
+                )
                 .csrf(CsrfConfigurer::disable)
                 .sessionManagement(sessionManagement -> sessionManagement
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
